856f66bb32ebdd0900c680e5044ae214f757811f
Nate <nglod33@gmail.com>
2021-11-29 18:24:11 -0500

    Classify shape works

--- Classify_Shape.m
+++ Classify_Shape.m
@@ -1,23 +1,23 @@
-function Classify_Shape( im )
+function r_shape = Classify_Shape( im )
 
     % get image into binary image
     gray_im = rgb2gray(im);
     bin_im = imbinarize(gray_im);
     se = strel('disk', 3);
-    
-    % dilate im
-    bin_im = imopen(bin_im, se);
-        
+    bin_im = imclose(bin_im, se);
+            
     % Edge detection
-    se = strel('disk', 5);
     edges = edge(bin_im, 'sobel');
     edges = imclose(edges, se);
     
     % Detect straight lines
     [H, T, R] = hough(edges);
     P = houghpeaks(H, 15);
-    lines = houghlines(edges, T, R, P, 'FillGap', 100, 'MinLength', 5);
+    lines = houghlines(edges, T, R, P, 'FillGap', 10, 'MinLength', 100);
     
+    % Code to plot out all the lines
+    %{
+    fprintf('Number of lines found: %d\n', length(lines));
     % Plot lines for dev purposes
     figure, imshow(edges), hold on
     max_len = 0;
@@ -36,8 +36,40 @@ function Classify_Shape( im )
             xy_long = xy;
         end
     end
+    %}
     
-    % Compare angles of lines to classify shapes
+    % Calculate slope of all lines, check if parallel
+    slopeArray = zeros([1, length(lines)]);
+    for k = 1:length(lines)
+        x1 = lines(k).point1(1);
+        y1 = lines(k).point1(2);
+        x2 = lines(k).point2(1);
+        y2 = lines(k).point2(2);
+        slopeArray(k) = (x2 - x1) / (y2 - y1);
+    end
     
+    % disp(slopeArray);
     
+    % If there are no lines, it's a squiggle
+    if (length(lines) < 1)
+        r_shape = "squiggle";
+    else
+        highest = 0;
+        lowest = 0;
+        for k = 1:length(slopeArray)
+            if slopeArray(k) > highest
+                highest = slopeArray(k);
+            end
+            if slopeArray(k) < lowest
+               lowest = slopeArray(k);
+            end
+        end
+        disp(highest);
+        disp(lowest);
+        if( ( highest > 0 && lowest < 0) && ( highest - lowest > 3))
+            r_shape = "diamond"; 
+        else
+            r_shape = "oval";
+        end
+    end
 end
